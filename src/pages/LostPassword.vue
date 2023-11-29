d03bd4b8d838b8ed0abdb824c5f5bfba63c695c5
Agoua David <cdavidagoua@gmail.com>
2023-11-29 18:52:02 +0000

    add pagination

---
+++ src/pages/LostPassword.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "LostPassword.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/RecherchePatient.vue
+++ src/pages/RecherchePatient.vue
@@ -1,75 +1,81 @@
 <template>
-<PageLayout page-title="Recherche Global" >
-<section class="card">
-  <div class="card-body">
-    <div class="d-flex mb-1 justify-content-between">
-      <div></div>
-      <div>
-        <Button class="ml-1" size="small" @click="goTo('/new-patient')">
-          <span class="ft-plus"></span>
-          Ajouter
-        </Button>
-      </div>
-    </div>
-    <div >
-      <form @submit.prevent="submit" class="row mb-2 align-items-end">
-        <div class="col-md-4 mt-1">
-          <label for="">Nom</label><br>
-          <InputText class="w-100" v-model="searchForm.nom"/>
-        </div>
-        <div class="col-md-4 mt-1">
-          <label for="">Prénoms</label><br>
-          <InputText class="w-100" v-model="searchForm.prenoms"/>
-        </div>
-        <div class="col-md-4 mt-1">
-          <label for="">Téléphone</label><br>
-          <InputNumber class="w-100" v-model="searchForm.contact"/>
-        </div>
-        <div class="col-md-4 mt-1">
-          <label for="">Code patient</label><br>
-          <InputText class="w-100" v-model="searchForm.code_patient"/>
-        </div>
-        <div class="col-md-4 mt-1">
-          <label for="">Date de naissance</label><br>
-          <InputText class="w-100" type="date" v-model="searchForm.date_naissance"/>
-        </div>
-        <div class="col-md-4 mt-1 text-right">
-          <Button type="submit">
-            <i class="pi pi-search mr-3"></i>
-            Rechercher
-          </Button>
-        </div>
-      </form>
-    </div>
-    <DataTable :filters="filters" :value="patients" data-key="id" :selection="selectedPatient"   :loading="patientLoading">
-      <template #loading>
-        <div class="text-center">Chargement des resultats</div>
-      </template>
-
-      <Column selection-mode="multiple" />
-      <Column field="code_patient" header="Code Patient">
-
-      </Column>
-      <Column field="nom" header="Nom"></Column>
-      <Column field="prenoms" header="Prénoms"></Column>
-      <Column field="contact" header="Contact"></Column>
-      <Column header="Actions">
-        <template class="d-flex" #body="slotProp">
-          <div class="d-flex">
-            <GeneralOptions :key="slotProp.data.id" :patient="slotProp.data"/>
-            <router-link class="p-button p-button-sm" size="small" :to="'/dossier/'+slotProp.data.id">
-              <span class="ft ft-folder"></span>
-              <span class="ml-1">Dossier</span>
-            </router-link>
-
+  <PageLayout page-title="Recherche Global">
+    <section class="card">
+      <div class="card-body">
+        <div class="d-flex mb-1 justify-content-between">
+          <div></div>
+          <div>
+            <Button class="ml-1" size="small" @click="goTo('/new-patient')">
+              <span class="ft-plus"></span>
+              Ajouter
+            </Button>
           </div>
-        </template>
-      </Column>
-    </DataTable>
-  </div>
-</section>
+        </div>
+        <div>
+          <form @submit.prevent="submit" class="row mb-2 align-items-end">
+            <div class="col-md-4 mt-1">
+              <label for="">Nom</label><br>
+              <InputText class="w-100" v-model="searchForm.nom"/>
+            </div>
+            <div class="col-md-4 mt-1">
+              <label for="">Prénoms</label><br>
+              <InputText class="w-100" v-model="searchForm.prenoms"/>
+            </div>
+            <div class="col-md-4 mt-1">
+              <label for="">Téléphone</label><br>
+              <InputNumber class="w-100" v-model="searchForm.contact"/>
+            </div>
+            <div class="col-md-4 mt-1">
+              <label for="">Code patient</label><br>
+              <InputText class="w-100" v-model="searchForm.code_patient"/>
+            </div>
+            <div class="col-md-4 mt-1">
+              <label for="">Date de naissance</label><br>
+              <InputText class="w-100" type="date" v-model="searchForm.date_naissance"/>
+            </div>
+            <div class="col-md-4 mt-1 text-right">
+              <Button type="submit">
+                <i class="pi pi-search mr-3"></i>
+                Rechercher
+              </Button>
+            </div>
+          </form>
+        </div>
+        <DataTable
+          :filters="filters"
+          :value="patients"
+          paginator :rows="rowsPerPage" :rows-per-page-options="[20,50,100,300]"
+          data-key="id"
+          :selection="selectedPatient"
+          :loading="patientLoading">
+          <template #loading>
+            <div class="text-center">Chargement des resultats</div>
+          </template>
 
-</PageLayout>
+          <Column selection-mode="multiple"/>
+          <Column field="code_patient" header="Code Patient">
+
+          </Column>
+          <Column field="nom" header="Nom"></Column>
+          <Column field="prenoms" header="Prénoms"></Column>
+          <Column field="contact" header="Contact"></Column>
+          <Column header="Actions">
+            <template class="d-flex" #body="slotProp">
+              <div class="d-flex">
+                <GeneralOptions :key="slotProp.data.id" :patient="slotProp.data"/>
+                <router-link class="p-button p-button-sm" size="small" :to="'/dossier/'+slotProp.data.id">
+                  <span class="ft ft-folder"></span>
+                  <span class="ml-1">Dossier</span>
+                </router-link>
+
+              </div>
+            </template>
+          </Column>
+        </DataTable>
+      </div>
+    </section>
+
+  </PageLayout>
 </template>
 
 <script setup>
@@ -80,7 +86,7 @@ import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import GeneralOptions from "../components/GeneralOptions.vue";
 import useMyFetch from "../compoables/useMyFetch.js";
-import {useToast}  from "primevue/usetoast";
+import {useToast} from "primevue/usetoast";
 import InputText from "primevue/inputtext";
 import {useRouter} from "vue-router";
 import InputNumber from 'primevue/inputnumber';
@@ -88,16 +94,21 @@ import InputNumber from 'primevue/inputnumber';
 
 const router = useRouter();
 const toast = useToast()
-let {data: patients, isFetching: patientLoading, statusCode, onFetchError: onPatientFetchError } = useMyFetch('patients/').json()
+const rowsPerPage = ref(20)
 
-onPatientFetchError((error)=>{
+let {
+  data: patients,
+  isFetching: patientLoading,
+  statusCode,
+  onFetchError: onPatientFetchError
+} = await useMyFetch('patients/').json()
 
-    toast.add({
-      severity: 'error',
-      detail: error
-    })
+onPatientFetchError((error) => {
+  toast.add({
+    severity: 'error',
+    detail: error
+  })
 })
-
 
 const filters = reactive({
   global: {
@@ -114,30 +125,28 @@ const search = ref("");
 const goTo = (path) => {
   router.push(path);
 };
-watch(patients, (patients)=>{
-  console.log('patients:'+ patients.length)
-})
 
-const searchForm= reactive({})
 
-const submit = async()=>{
-  try{
+const searchForm = reactive({})
+
+const submit = async () => {
+  try {
     let querypath = `code_patient=${searchForm.code_patient ?? ''}&nom=${searchForm.nom ?? ''}&prenoms=${searchForm.prenoms ?? ''}&contact=${searchForm.contact ?? ''}`
-    let {data, isFetching, isFinished, error } = await useMyFetch('patients/?'+querypath).json()
+    let {data, isFetching, isFinished, error} = await useMyFetch('patients/?' + querypath).json()
     patientLoading = isFetching
-    if(isFinished.value){
-      patients.value = data.value
+    if (isFinished.value) {
+      patients.value = data.value.results
     }
-    if(error.value){
+    if (error.value) {
       toast.add({
         severity: 'error',
         detail: "Error: " + error.value,
         life: 3000
       })
     }
-  }catch (e) {
+  } catch (e) {
     console.log(e)
-  }finally {
+  } finally {
     patientLoading.value = false
   }
 
